15d24fa2a3bf0ef18cc10003553339e9e27abf82
Joanna Panimasz <pani.joanna93@gmail.com>
2023-02-02 14:18:16 +0100

    add Container inside Header

--- src/features/Header/index.js
+++ src/features/Header/index.js
@@ -1,19 +1,28 @@
 import photo from "./photo.jpg";
-import { MainParagraph, Image, Heading, Paragraph, Button } from "./styled";
+import {
+  MainParagraph,
+  Image,
+  Heading,
+  Paragraph,
+  Button,
+  Container,
+} from "./styled";
 import { Wrapper } from "../../common/Wrapper/styled.js";
 
 const Header = () => {
   return (
     <Wrapper>
       <Image src={photo} alt="Joanna Panimasz" />
-      <Paragraph>This is</Paragraph>
-      <Heading>Joanna Panimasz</Heading>
-      <MainParagraph>
-        👩‍💻Passionate about Frontend technologies (React). Frontend is always
-        evolving and presents new challenges, which makes it an exciting and
-        rewarding field to be in.🚀
-      </MainParagraph>
-      <Button>Hire me</Button>
+      <Container>
+        <Paragraph>This is</Paragraph>
+        <Heading>Joanna Panimasz</Heading>
+        <MainParagraph>
+          👩‍💻Passionate about Frontend technologies (React). Frontend is always
+          evolving and presents new challenges, which makes it an exciting and
+          rewarding field to be in.🚀
+        </MainParagraph>
+        <Button>Hire me</Button>
+      </Container>
     </Wrapper>
   );
 };
