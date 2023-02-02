592221b037c2eafe9f6cc33223a2bb1725b2c423
Joanna Panimasz <pani.joanna93@gmail.com>
2023-02-02 14:11:13 +0100

    add compnents inside the Header

--- src/features/Header/index.js
+++ src/features/Header/index.js
@@ -1,7 +1,21 @@
 import photo from "./photo.jpg";
+import { MainParagraph, Image, Heading, Paragraph, Button } from "./styled";
+import { Wrapper } from "../common/Wrapper/styled.js";
 
 const Header = () => {
-
-}
+  return (
+    <Wrapper>
+      <Image src={photo} alt="Joanna Panimasz" />
+      <Paragraph>This is</Paragraph>
+      <Heading>Joanna Panimasz</Heading>
+      <MainParagraph>
+        👩‍💻Passionate about Frontend technologies (React). Frontend is always
+        evolving and presents new challenges, which makes it an exciting and
+        rewarding field to be in.🚀
+      </MainParagraph>
+      <Button>Hire me</Button>
+    </Wrapper>
+  );
+};
 
 export default Header;
