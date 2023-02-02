6252fcb34c3f6462146cf8690fcd0218a323ff13
Joanna Panimasz <pani.joanna93@gmail.com>
2023-02-02 13:56:14 +0100

    export MainParagraph to the styled.js

--- src/features/Header/styled.js
+++ src/features/Header/styled.js
@@ -7,21 +7,32 @@ export const Image = styled.img`
 `;
 
 export const Paragraph = styled.p`
-font-family: 'Inter';
-font-style: normal;
-font-weight: 700;
-font-size: 12px;
-line-height: 130%;
-text-transform: uppercase;
-color: #6E7E91;
+  font-family: "Inter";
+  font-style: normal;
+  font-weight: 700;
+  font-size: 12px;
+  line-height: 130%;
+  text-transform: uppercase;
+  color: #6e7e91;
+`;
+
+export const MainParagraph = styled.p`
+  font-family: "Inter";
+  font-style: normal;
+  font-weight: 400;
+  font-size: 20px;
+  line-height: 140%;
+  letter-spacing: 0.05em;
+  color: #6e7e91;
+  max-width: 600px;
 `;
 
 export const Heading = styled.h1`
-font-family: 'Inter';
-font-style: normal;
-font-weight: 900;
-font-size: 38px;
-line-height: 46px;
-letter-spacing: 0.05em;
-color: #252525;
+  font-family: "Inter";
+  font-style: normal;
+  font-weight: 900;
+  font-size: 38px;
+  line-height: 46px;
+  letter-spacing: 0.05em;
+  color: #252525;
 `;
