dd44e3f7e5f3b78139a64069e1996d23699cce76
Joanna Panimasz <pani.joanna93@gmail.com>
2023-02-06 15:03:07 +0100

    export StyledSection in the Section

---
+++ src/common/Section/styled.js
@@ -0,0 +1,12 @@
+import styled from "styled-components";
+
+export const StyledSection = styled.section`
+  padding: 0 5px 10px 0;
+  background-color: #ffffff;
+  width: 1000px;
+  height: 400px;
+  margin: 0 auto;
+  box-shadow: 0px -2px 50px rgba(9, 10, 51, 0.02),
+    0px 16px 58px rgba(9, 10, 51, 0.03);
+  border-radius: 4px;
+`;
