316b9f72268716b733ce5b86097c28c7957ae23b
Joanna Panimasz <pani.joanna93@gmail.com>
2023-02-06 15:07:41 +0100

    add Section to the App.js

--- src/App.js
+++ src/App.js
@@ -1,8 +1,13 @@
 import Header from "./features/Header";
+import Section from "./common/Section";
 
 function App() {
   return (
+    <>
       <Header />
+      <Section title="My skillset inculded" />
+      <Section title="What I want to learn next" />
+    </>
   );
 }
 
