a0f3e14f90ff262c4e2f481deef37344427a4834
Joanna Panimasz <pani.joanna93@gmail.com>
2023-02-08 12:54:35 +0100

    change grid to flexbox in Wrapper

--- src/common/Wrapper/styled.js
+++ src/common/Wrapper/styled.js
@@ -1,8 +1,7 @@
 import styled from "styled-components";
 
 export const Wrapper = styled.div`
-  display: grid;
-  grid-template-columns: auto 1fr;
-  grid-gap: 66px;
-  align-items: center;
+  display: flex;
+  justify-content: center;
+  flex-direction: column;
 `;
