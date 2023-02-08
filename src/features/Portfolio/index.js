d6b91cdcbc2df2a5c187f0bc592e03d690e243e7
Joanna Panimasz <pani.joanna93@gmail.com>
2023-02-08 11:04:38 +0100

    add components with props to the Portfolio

---
+++ src/features/Portfolio/index.js
@@ -0,0 +1,16 @@
+import GithubIcon from "../Icons/GithubIcon";
+import Heading from "../../common/Heading";
+import { Wrapper } from "../../common/Wrapper/styled";
+import Paragraph from "../../common/Paragraph";
+
+const Portfolio = () => {
+  return (
+    <Wrapper>
+      <GithubIcon />
+      <Heading title="Portfolio" />
+      <Paragraph primary body="My recent projects" />
+    </Wrapper>
+  );
+};
+
+export default Portfolio;
