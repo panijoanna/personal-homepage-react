483ac8dd6b43f9eb903a65f041faab510f2fb479
Joanna Panimasz <pani.joanna93@gmail.com>
2023-02-08 13:24:11 +0100

    add color, size props to the GithubIcon in Portfolio

--- src/features/Portfolio/index.js
+++ src/features/Portfolio/index.js
@@ -6,7 +6,7 @@ import Paragraph from "../../common/Paragraph";
 const Portfolio = () => {
   return (
     <Wrapper>
-      <GithubIcon />
+      <GithubIcon color="#0366D6" size="40" />
       <Heading title="Portfolio" />
       <Paragraph body="My recent projects" />
     </Wrapper>
