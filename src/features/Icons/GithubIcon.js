7a62d0dc8799efd53bd8077baef87ca796311ab2
Joanna Panimasz <pani.joanna93@gmail.com>
2023-02-08 13:24:22 +0100

    add size props in GithubIcon

--- src/features/Icons/GithubIcon.js
+++ src/features/Icons/GithubIcon.js
@@ -1,9 +1,9 @@
-const GithubIcon = ({ color }) => {
+const GithubIcon = ({ color, size }) => {
   return (
     <svg
       xmlns="http://www.w3.org/2000/svg"
       viewBox="0 0 24 24"
-      width="48"
+      width={size}
       height="48"
     >
       <g fill={color}>
