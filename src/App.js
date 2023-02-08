dbe0daa31bdfd56bbbb62a07cf1d81e34c9787a5
Joanna Panimasz <pani.joanna93@gmail.com>
2023-02-08 12:48:45 +0100

    add Wrapper instead React Fragment in App.js

--- src/App.js
+++ src/App.js
@@ -1,15 +1,16 @@
 import Header from "./features/Header";
 import Section from "./common/Section";
 import Portfolio from "./features/Portfolio";
+import { Wrapper } from "./common/Wrapper/styled";
 
 function App() {
   return (
-    <>
+    <Wrapper>
       <Header />
       <Section title="My skillset inculded" />
       <Section title="What I want to learn next" />
       <Portfolio />
-    </>
+    </Wrapper>
   );
 }
 
