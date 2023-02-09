f9881c1c1251f0f117fc6a83430facda40ece3f2
Joanna Panimasz <pani.joanna93@gmail.com>
2023-02-09 11:20:24 +0100

    fix styles in the StyledSection

--- src/common/Section/styled.js
+++ src/common/Section/styled.js
@@ -1,10 +1,12 @@
 import styled from "styled-components";
 
 export const StyledSection = styled.section`
-  padding: 0 5px 10px 0;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 80vh;
+  width: 190vh;
   background-color: #ffffff;
-  width: 1000px;
-  height: 400px;
   margin: 0 auto;
   box-shadow: 0px -2px 50px rgba(9, 10, 51, 0.02),
     0px 16px 58px rgba(9, 10, 51, 0.03);
