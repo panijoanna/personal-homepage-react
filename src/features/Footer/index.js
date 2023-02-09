01edb833c86e92a2ab42b22e306fe4b5f9d876a3
Joanna Panimasz <pani.joanna93@gmail.com>
2023-02-09 11:21:00 +0100

    add FooterContainer component to the Footer

--- src/features/Footer/index.js
+++ src/features/Footer/index.js
@@ -2,7 +2,7 @@ import {
   EmailParagraph,
   UppercaseParagraph,
 } from "../../common/Paragraph/styled";
-import { StyledFooter } from "./styled";
+import { StyledFooter, FooterContainer } from "./styled";
 import Paragraph from "../../common/Paragraph";
 import GithubIcon from "../Icons/GithubIcon";
 import LinkedinIcon from "../Icons/LinkedinIcon";
@@ -11,6 +11,7 @@ import { Link } from "../Header/styled";
 const Footer = () => {
   return (
     <StyledFooter>
+      <FooterContainer>
       <UppercaseParagraph>Let's talk</UppercaseParagraph>
       <EmailParagraph>p.joanna93@gmail.com</EmailParagraph>
       <Paragraph body="I am a dedicated and passionate developer ready to tackle new challenges and bring creative solutions to your projects. Don't hesitate to contact me if you're in need of a hard-working and innovative developer to help bring ideas to life." />
@@ -23,6 +24,7 @@ const Footer = () => {
       >
         <LinkedinIcon />
       </Link>
+      </FooterContainer>
     </StyledFooter>
   );
 };
