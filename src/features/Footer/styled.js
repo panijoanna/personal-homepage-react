0fb1129d4405273fb8ed9578f9b7f2abe6143ead
Joanna Panimasz <pani.joanna93@gmail.com>
2023-02-09 11:20:44 +0100

    add styles to the StyledFooter

--- src/features/Footer/styled.js
+++ src/features/Footer/styled.js
@@ -1,3 +1,15 @@
 import styled from "styled-components";
 
-export const StyledFooter = styled.footer``;
+export const StyledFooter = styled.footer`
+display: flex;
+flex-direction: column;
+align-items: center;
+height: 80vh;
+width: 135vh;
+`;
+
+export const FooterContainer = styled.div`
+
+`;
+
+
