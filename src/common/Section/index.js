9e3f9d7d88327fdd75842b9c38c8d59b8fe26261
Joanna Panimasz <pani.joanna93@gmail.com>
2023-02-10 11:43:09 +0100

    add components to the Section

--- src/common/Section/index.js
+++ src/common/Section/index.js
@@ -1,10 +1,17 @@
-import { StyledSection } from "./styled";
+import { ItemList, List, SectionContainer, StyledSection } from "./styled";
 import { StyledHeading } from "../Heading/styled";
 
-const Section = ({ title }) => {
+const Section = ({ title, array }) => {
   return (
     <StyledSection>
       <StyledHeading>{title}</StyledHeading>
+      <SectionContainer>
+        <List>
+          {array.map((element) => (
+            <ItemList key={element}>{element}</ItemList>
+          ))}
+        </List>
+      </SectionContainer>
     </StyledSection>
   );
 };
