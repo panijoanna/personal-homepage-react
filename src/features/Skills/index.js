9f03ad88d447e1b62512a3b35e785d35ab8ce5bc
Joanna Panimasz <pani.joanna93@gmail.com>
2023-02-10 11:43:42 +0100

    add Sections to the Skills component

--- src/features/Skills/index.js
+++ src/features/Skills/index.js
@@ -1,8 +1,14 @@
+import Section from "../../common/Section/index";
+import { skillset } from "./skillset/skillset";
+import { skillsToLearn } from "./skillset/skillsToLearn";
 
 const Skills = () => {
-return (
-    
-)
+  return (
+    <>
+      <Section title="My skillset included" array={skillset} />
+      <Section title="What I want to learn next" array={skillsToLearn} />
+    </>
+  );
 };
 
 export default Skills;
