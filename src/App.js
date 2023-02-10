a94ba1d16500e1cfe4f8de68b175400fa4a06b17
Joanna Panimasz <pani.joanna93@gmail.com>
2023-02-10 11:37:11 +0100

    change Sections to Skills component in App.js

--- src/App.js
+++ src/App.js
@@ -1,15 +1,14 @@
 import Header from "./features/Header";
-import Section from "./common/Section";
 import Portfolio from "./features/Portfolio";
 import { Wrapper } from "./common/Wrapper/styled";
 import Footer from "./features/Footer";
+import Skills from "./features/Skills";
 
 function App() {
   return (
     <Wrapper>
       <Header />
-      <Section title="My skillset inculded" />
-      <Section title="What I want to learn next" />
+      <Skills />
       <Portfolio />
       <Footer />
     </Wrapper>
