7127a09488d54d682691cbf6cba49e4e6e6f7250
Joanna Panimasz <pani.joanna93@gmail.com>
2023-03-08 15:43:56 +0100

    add colors to the theme

--- src/theme.js
+++ src/theme.js
@@ -6,6 +6,8 @@ export const theme = {
     gray: "#6e7e91",
     athensGray: "#F1F2F4",
     lightGray: "#F7F7F9",
+    whiteLilac: "#fbfbfe",
+    manatee: "#8a899b",
   },
   breakpoint: {
     mobileDevice: 767,
