0db70097233a0c5d9299d35c1ddb0fd9ec6ae8fd
Joanna Panimasz <pani.joanna93@gmail.com>
2023-03-08 15:24:55 +0100

    add color to the theme

--- src/theme.js
+++ src/theme.js
@@ -1,5 +1,12 @@
 export const theme = {
-  color: {},
+  color: {
+    mineShaft: "#252525",
+    blue: "#0366d6",
+    white: "#ffffff",
+    gray: "#6e7e91",
+    athensGray: "#F1F2F4",
+    lightGray: "#F7F7F9",
+  },
   breakpoint: {
     mobileDevice: 767,
   },
