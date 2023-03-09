694d318d69436912619757c5a0d7bce88533fc1e
Joanna Panimasz <pani.joanna93@gmail.com>
2023-03-09 12:48:26 +0100

    create PortfolioSection in styled

--- src/features/Portfolio/styled.js
+++ src/features/Portfolio/styled.js
@@ -1,5 +1,7 @@
 import styled from "styled-components";
 
+export const PortfolioSection = styled.section``;
+
 export const PortfolioContainer = styled.div`
   display: flex;
   justify-content: flex-start;
