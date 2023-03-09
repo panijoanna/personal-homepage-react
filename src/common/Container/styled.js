75f0f9dba42c9d4c51e37532ad50fe6520bd1b92
Joanna Panimasz <pani.joanna93@gmail.com>
2023-03-09 11:17:22 +0100

    add styles to the PortfolioBox

--- src/common/Container/styled.js
+++ src/common/Container/styled.js
@@ -4,4 +4,8 @@ export const PortfolioBox = styled.div`
   color: ${({ theme }) => theme.color.white};
   width: 592px;
   height: 322px;
+  border: 6px solid ${({ theme }) => theme.color.athensGray};
+  box-shadow: 0px -2px 50px ${({ theme }) => theme.color.athensGray};
+    0px 16px 58px ${({ theme }) => theme.color.lightGray};
+  border-radius: 4px;
 `;
