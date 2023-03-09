43175a1cf350e1e499b437828e82bdcefafe3375
Joanna Panimasz <pani.joanna93@gmail.com>
2023-03-09 11:51:47 +0100

    change px to vh in PortfolioBox

--- src/common/Container/styled.js
+++ src/common/Container/styled.js
@@ -2,7 +2,7 @@ import styled from "styled-components";
 
 export const PortfolioBox = styled.div`
   color: ${({ theme }) => theme.color.white};
-  width: 592px;
+  width: 95vh;
   height: 322px;
   border: 6px solid ${({ theme }) => theme.color.athensGray};
   box-shadow: 0px -2px 50px ${({ theme }) => theme.color.athensGray};
