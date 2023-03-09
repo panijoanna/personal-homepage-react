19c531867611452d2dcdc2b5801f0bf44583d0d2
Joanna Panimasz <pani.joanna93@gmail.com>
2023-03-09 12:50:27 +0100

    add margin to the PortfolioSection

--- src/features/Portfolio/styled.js
+++ src/features/Portfolio/styled.js
@@ -1,6 +1,8 @@
 import styled from "styled-components";
 
-export const PortfolioSection = styled.section``;
+export const PortfolioSection = styled.section`
+  margin: 0 auto;
+`;
 
 export const PortfolioContainer = styled.div`
   display: flex;
