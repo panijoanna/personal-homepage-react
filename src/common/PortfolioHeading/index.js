dae4f7009afaf39af995859aca560a569eda5571
Joanna Panimasz <pani.joanna93@gmail.com>
2023-03-09 11:05:17 +0100

    create PortfolioHeading

---
+++ src/common/PortfolioHeading/index.js
@@ -0,0 +1,7 @@
+import { StyledPortfolioHeading } from "./styled";
+
+const PortfolioHeading = ({ title }) => {
+  return <StyledPortfolioHeading>{title}</StyledPortfolioHeading>;
+};
+
+export default PortfolioHeading;
