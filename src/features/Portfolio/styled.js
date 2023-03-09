98e72a3240a30c4bf2f3daa5a886a04c22f3bcdf
Joanna Panimasz <pani.joanna93@gmail.com>
2023-03-09 11:52:16 +0100

    create Wrap in styled

--- src/features/Portfolio/styled.js
+++ src/features/Portfolio/styled.js
@@ -5,7 +5,16 @@ export const PortfolioContainer = styled.div`
   justify-content: flex-start;
   flex-direction: column;
   align-items: center;
-  height: 80vh;
+  height: 52vh;
   margin: 0 auto;
   padding-top: 72px;
+`;
+
+export const Wrap = styled.div`
+  display: grid;
+  grid-template-columns: repeat(2, 1fr);
+  grid-template-rows: repeat(2, 1fr);
+  grid-column-gap: 32px;
+  grid-row-gap: 32px;
+  margin: 0 auto;
 `;
