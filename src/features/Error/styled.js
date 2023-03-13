5607e20331152486205f729671387ef6f6675bd0
Joanna Panimasz <pani.joanna93@gmail.com>
2023-03-13 12:15:23 +0100

    create ErrorParagraph to the styled

--- src/features/Error/styled.js
+++ src/features/Error/styled.js
@@ -5,6 +5,22 @@ export const ErrorMessage = styled.h3`
   color: ${({ theme }) => theme.color.mineShaft};
   font-weight: 700;
   line-height: 29px;
+  margin: 0;
+  padding-top: 23px;
 `;
 
-export const ErrorContainer = styled.div``;
+export const ErrorContainer = styled.div`
+display: flex;
+justify-content: center;
+text-align: center;
+flex-direction: column;
+`;
+
+export const ErrorParagraph = styled.p`
+font-family: 'Inter';
+font-style: normal;
+font-weight: 400;
+font-size: 20px;
+line-height: 140%;
+letter-spacing: 0.05em;
+`;
