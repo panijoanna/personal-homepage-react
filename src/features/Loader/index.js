6f7fae04ae2e40a04d833481e9e60036b0870571
Joanna Panimasz <pani.joanna93@gmail.com>
2023-03-13 12:43:19 +0100

    rename Loading to a Loader

---
+++ src/features/Loader/index.js
@@ -0,0 +1,11 @@
+import { StyledParagraph } from "../../common/Paragraph/styled";
+
+
+const Loader = () => {
+return (
+    <StyledParagraph body="Please, wait, projects are being loaded..."/>
+    
+)
+};
+
+export default Loader;
--- src/features/Loading/index.js
+++
@@ -1,7 +0,0 @@
-
-
-const Loading = () => {
-
-};
-
-export default Loading;
