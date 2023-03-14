c90d10c105da4357756c0c23e84c495ee48688d2
Joanna Panimasz <pani.joanna93@gmail.com>
2023-03-14 13:46:56 +0100

    create ThemeButton

--- src/features/ModeButton/styled.js
+++ src/features/ModeButton/styled.js
@@ -1,1 +1,9 @@
 import styled from "styled-components";
+
+export const ThemeButton = styled.button`
+  color: ${({ theme }) => theme.color.mercury};
+  border: 1px solid ${({ theme }) => theme.color.gray};
+  width: 47px;
+  height: 25px;
+  border-radius: 30px;
+`;
