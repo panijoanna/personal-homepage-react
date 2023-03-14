40d70620221b1ed2f5822fa7af32c4b1fdca8186
Joanna Panimasz <pani.joanna93@gmail.com>
2023-03-14 09:43:50 +0100

    add loading state to the useRepos custom hook

--- src/features/Portfolio/useRepos.js
+++ src/features/Portfolio/useRepos.js
@@ -5,10 +5,12 @@ import { useEffect, useState } from "react";
 const useRepos = () => {
   const [repos, setRepos] = useState([]);
   const [error, setError] = useState(null);
+  const [isLoading, setIsLoading] = useState(false);
 
   useEffect(() => {
     const fetchData = async () => {
       try {
+        setIsLoading(true);
         const response = await axios.get(
           "https://api.github.com/users/panijoanna/repos"
         );
@@ -16,10 +18,13 @@ const useRepos = () => {
       } catch (error) {
         setError(<Error />);
       }
+      setTimeout(() => {
+        setIsLoading(false);
+      }, 1000);
     };
     fetchData();
   }, []);
-  return { repos, error };
+  return { repos, error, isLoading };
 };
 
 export default useRepos;
