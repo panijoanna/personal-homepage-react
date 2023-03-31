ffcf429450794596f8bf74d3eaed1bbe80f7faed
Joanna Panimasz <pani.joanna93@gmail.com>
2023-03-31 12:09:25 +0200

    add sliding functionality to the button

--- src/features/ThemeSwitcher/index.js
+++ src/features/ThemeSwitcher/index.js
@@ -1,11 +1,17 @@
+import { useState } from "react";
 import { ThemeButton, ThemeWrapper, ThemeToggleIcon } from "./styled";
 
 const ThemeSwitcher = () => {
+  const [darkTheme, setDarktheme] = useState(false);
+
+  const handleClick = () => {
+    setDarktheme(!darkTheme);
+  };
 
   return (
     <ThemeWrapper>
-      <ThemeButton>
-        <ThemeToggleIcon />
+      <ThemeButton onClick={handleClick}>
+        <ThemeToggleIcon move={darkTheme} />
       </ThemeButton>
     </ThemeWrapper>
   );
