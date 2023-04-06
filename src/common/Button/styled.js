a7f80b5580e5f4f7904f93d2a5a431b8b2a3854d
Joanna Panimasz <pani.joanna93@gmail.com>
2023-04-06 17:39:06 +0200

    add theme to font-weight in Button

--- src/common/Button/styled.js
+++ src/common/Button/styled.js
@@ -7,7 +7,7 @@ export const Button = styled.button`
   padding: 12px 16px;
   gap: 16px;
   font-family: "Inter";
-  font-weight: 600;
+  font-weight: ${({ theme }) => theme.fontWeights.medium};
   background: ${({ theme }) => theme.color.blue};
   color: ${({ theme }) => theme.color.white};
   border: 1px solid ${({ theme }) => theme.color.athensGray};
