99986cfc3afbd71d952146825497d29539b8a89d
Joanna Panimasz <pani.joanna93@gmail.com>
2023-06-19 16:30:31 +0200

    shorten ThemeSwitcher

--- src/features/ThemeSwitcher/index.js
+++ src/features/ThemeSwitcher/index.js
@@ -1,26 +1,14 @@
-import { useState, useContext, useEffect } from "react";
+import { useContext } from "react";
 import { ThemeButton, ThemeWrapper, ThemeToggleIcon } from "./styled";
 import { ThemeContext } from "styled-components";
-import { getTasksFromLocalStorage } from "./themeLocalStorage";
 
 const ThemeSwitcher = () => {
-  const [changeTheme, setChangeTheme] = useState(false);
-  const { themeToggler } = useContext(ThemeContext);
-
-  useEffect(() => {
-    getTasksFromLocalStorage();
-    setChangeTheme(getTasksFromLocalStorage() === "dark");
-  }, []);
-
-  const handleClick = () => {
-    setChangeTheme(!changeTheme);
-    themeToggler();
-  };
+  const { theme, themeToggler } = useContext(ThemeContext);
 
   return (
     <ThemeWrapper>
-      <ThemeButton onClick={handleClick}>
-        <ThemeToggleIcon move={changeTheme} />
+      <ThemeButton onClick={themeToggler}>
+        <ThemeToggleIcon move />
       </ThemeButton>
     </ThemeWrapper>
   );
